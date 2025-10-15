5f3b1d856001cd7568616d354e3efd10b24e0d74
Dimitar <dimitar.mandarliev@gmail.com>
2025-10-15 14:28:04 +0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip? </h3>😍
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer>
+      💼 You have X items on your list, and you already packed X (X%)
+    </footer>
+  );
+}
